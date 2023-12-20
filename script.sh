1af23d2e451523ecb6fe191362df43a3b8d84093
VPSGRID <154324502+VPSGRID@users.noreply.github.com>
2023-12-20 12:27:29 +1000

    Update script.sh
    Shortened the code and added some more features such as saying if you're not in root, please be in root

--- script.sh
+++ script.sh
@@ -1,56 +1,31 @@
 #!/bin/bash
 
-# Function to display an error message and exit
-function display_error() {
-    echo "Error: $1"
-    exit 1
-}
+# This script sets up a remote desktop connection using xrdp and ngrok
 
-# Function to prompt user for confirmation
-function prompt_confirmation() {
-    read -p "$1 (y/n): " response
-    if [[ "$response" =~ ^[Yy]$ ]]; then
-        return 0  # User confirmed
-    else
-        return 1  # User did not confirm
-    fi
-}
+# Exit if not run as root
+[ "$EUID" -eq 0 ] || { echo "Error: This script must be run as root. Please use sudo."; exit 1; }
 
-# Check if the script is run as root
-if [ "$EUID" -ne 0 ]; then
-    display_error "This script must be run as root. Please use sudo."
-fi
-
-# Display a warning and prompt for confirmation
+# Prompt user for confirmation and root password
 echo "This script will install xrdp, ngrok, change the root password, and launch xrdp on port 3389."
-prompt_confirmation "Do you want to continue?"
-
-# Prompt user for root password
+read -p "Do you want to continue? (y/n): " response
+[ "$response" = "y" ] || exit 0
 read -s -p "Enter the new root password: " root_password
 echo
 
-# Change root password
+# Change root password and install xrdp
 echo "root:$root_password" | chpasswd
-
-# Install xrdp
 apt-get update
 apt-get install -y xrdp
 
-# Check if ngrok is already present
-if ! command -v ngrok &> /dev/null; then
-    prompt_confirmation "Ngrok is not installed. Do you want to download it?"
-
-    # Download and extract ngrok with --no-check-certificate option
+# Download ngrok if not present and prompt user for authtoken
+command -v ngrok &> /dev/null || { 
+    echo "Ngrok is not installed. Downloading it now."
     wget --no-check-certificate https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
     tar -xzf ngrok-v3-stable-linux-amd64.tgz
     rm ngrok-v3-stable-linux-amd64.tgz
-fi
-
-# Prompt user for ngrok authtoken
+}
 read -p "Enter your ngrok authtoken: " authtoken
 
-# Set ngrok authtoken and launch ngrok in the background
+# Launch ngrok and xrdp in the background
 ./ngrok authtoken "$authtoken" && ./ngrok tcp 3389 &
-
-# Start xrdp in the background
 xrdp &
